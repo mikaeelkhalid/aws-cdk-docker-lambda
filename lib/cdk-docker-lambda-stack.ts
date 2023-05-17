192e1d4714479f4c529460e582d32feb3302a8d9
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-05-17 06:21:47 +0500

    update imports

--- lib/cdk-docker-lambda-stack.ts
+++ lib/cdk-docker-lambda-stack.ts
@@ -1,15 +1,15 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
 import path = require('path');
 
 export class CdkDockerLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new lambda.DockerImageFunction(this, 'docker-lambda-function', {
+    new DockerImageFunction(this, 'docker-lambda-function', {
       functionName: "docker-lambda-fn",
-      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../lambdas')),
+      code: DockerImageCode.fromImageAsset(path.join(__dirname, '../lambdas')),
       memorySize: 512,
       timeout: Duration.minutes(2),
       description: "This is dockerized AWS Lambda function"
